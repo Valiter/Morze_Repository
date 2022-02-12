2618b6266535b57fa7cc79fc2a26c56f7bf25543
antonkuzmichev <an2000n@icloud.com>
2022-02-13 00:31:20 +0300

    upd: [00:31, 13.02.22] comment: Устал и ничего не понял. Вопрос вывода звука остается открытым... Так же, как и вопрос генерации, этого чертового звука. Не имею ни малейшего понятия как с этим разобраться! :(
    Task's:
    - Готово! (1) Удивительно, но Py Audio скачалось нормально! Проверить работу.
    - (2) Сделать словари звуков и букв.
    - (3) Найти правила морзянки.
    - (5) Понять команды для ввода и вывода информации. Необхдимо понять:
    - (5.1) Как записывать звук и проигрывать его.
    - (5.2) Как генерировать звук программно, что важней.
    - (4)Если PyAudio Работает нормально(ред.(см.пункт 5), то сделать 2 варианта для "озвучки":
    - (4.1) Вариант "стандарт" с обычной морзянкой.
    - (4.2) Вариант "модуляция" с перекладыванием "точка/тире/пауза" на уровень тона, что позволяет не тратить время на на долгие звуки "тира/пауза".

--- audio_maker.py
+++ audio_maker.py
@@ -1,45 +1,42 @@
+"""PyAudio Example: Play a wave file (callback version)."""
+
 import pyaudio
 import wave
+import time
+import sys
 
+if len(sys.argv) < 2:
+    print("Plays a wave file.\n\nUsage: %s filename.wav" % sys.argv[0])
+    sys.exit(-1)
 
+wf = wave.open(sys.argv[1], 'rb')
+
+# instantiate PyAudio (1)
 p = pyaudio.PyAudio()
-for i in range(p.get_device_count()):
-    print(i, p.get_device_info_by_index(i)['name'])
 
+# define callback (2)
+def callback(in_data, frame_count, time_info, status):
+    data = wf.readframes(frame_count)
+    return (data, pyaudio.paContinue)
 
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
-p = pyaudio.PyAudio()
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                input_device_index=2,
-                frames_per_buffer=CHUNK)
+# open stream using callback (3)
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True,
+                stream_callback=callback)
 
-print("* recording")
-frames = []
+# start the stream (4)
+stream.start_stream()
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
-print("* done recording")
+# wait for stream to finish (5)
+while stream.is_active():
+    time.sleep(0.1)
+
+# stop stream (6)
 stream.stop_stream()
 stream.close()
-p.terminate()
-
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
 wf.close()
 
-import pyaudio
-p = pyaudio.PyAudio()
-for i in range(p.get_device_count()):
-    print(p.get_device_info_by_index(i)['name'])
+# close PyAudio (7)
+p.terminate()
